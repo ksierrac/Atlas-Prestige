269eb7f18683de5c40b312abb15ea32c5a73b07d
ksierrac <kscockerill@gmail.com>
2016-04-13 12:27:51 -0400

    fixed missing line

--- MyApplication/app/src/main/java/com/example/bryan/myapplication/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/bryan/myapplication/MapsActivity.java
@@ -198,6 +198,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
 
                 if (!v.isSelected()) {
+                    v.setSelected(true);
                     addFoodMarkersToMap(diningCoords);
                 } else {
                     v.setSelected(false);
